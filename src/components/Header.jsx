d5b719b0c53aea84a42060c64ed0c1f54a97c167
Sercort <sergio.cortes18@inacapmail.cl>
2026-07-10 00:41:24 -0400

    estructura inicial del proyecto (Vite y componentes base)

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Multiverso DB</h1>
+      <p className="integrantes">
+        Integrantes: [Sergio Cortes] 
+      </p>
+    </header>
+  )
+}
+
+export default Header
